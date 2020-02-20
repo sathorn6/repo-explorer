6735af5f3fd73400bb8307cbb4db07d1d1fa753a
sathorn6 <sathorn6@users.noreply.github.com>
2020-02-20 22:22:17 +0100

    Initial commit

---
+++ src/git.ts
@@ -0,0 +1,171 @@
+import * as git from "isomorphic-git";
+import LightningFS from "@isomorphic-git/lightning-fs";
+
+const fs = new LightningFS("fs", { wipe: true });
+git.plugins.set("fs", fs);
+const pfs = fs.promises;
+
+const dir = "/myrepo";
+
+export interface TreeNode {
+	name: string;
+	type: "file" | "directory";
+	changes: number;
+	children: TreeNode[];
+}
+
+export const followPath = (tree: TreeNode, path: string): TreeNode | null => {
+	if (path === "") {
+		return tree;
+	}
+
+	const dirs = path.split("/");
+
+	let current: TreeNode | undefined = tree;
+	for (const dir of dirs) {
+		current = current.children.find(node => node.name === dir);
+		if (!current) {
+			return null;
+		}
+	}
+
+	return current;
+};
+
+export const analyzeRepo = async (repoUrl: string): Promise<TreeNode> => {
+	await pfs.mkdir(dir);
+
+	console.time("clone");
+	await git.clone({
+		dir,
+		corsProxy: "https://cors.isomorphic-git.org",
+		url: repoUrl,
+		ref: "master",
+		singleBranch: true
+	});
+	console.timeEnd("clone");
+
+	const changes = new Map<string, number>();
+	const countChange = (path: string) => {
+		changes.set(path, (changes.get(path) || 0) + 1);
+	};
+
+	const compareTrees = async (aId: string, bId: string, prefix = ["/"]) => {
+		if (aId === bId) {
+			// Trees are identical
+			return 0;
+		}
+
+		const aTree = await git.readTree({ dir, oid: aId });
+		const bTree = await git.readTree({ dir, oid: bId });
+
+		if (aTree.tree.some(({ type }) => type !== "tree" && type !== "blob")) {
+			throw new Error("unknown type");
+		}
+		if (bTree.tree.some(({ type }) => type !== "tree" && type !== "blob")) {
+			throw new Error("unknown type");
+		}
+
+		const aTrees = aTree.tree.filter(({ type }) => type === "tree");
+		const bTrees = bTree.tree.filter(({ type }) => type === "tree");
+		const aBlobs = aTree.tree.filter(({ type }) => type === "blob");
+		const bBlobs = bTree.tree.filter(({ type }) => type === "blob");
+
+		// FIXME: We don't treat the creation or deletion of a file as a change
+
+		for (const blob of aBlobs) {
+			const inB = bBlobs.find(({ path }) => path === blob.path);
+
+			if (!inB) {
+				// File was created, we don't count it
+				continue;
+			}
+
+			if (blob.oid !== inB.oid) {
+				for (const dir of prefix) {
+					countChange(dir);
+				}
+				countChange(`${prefix[prefix.length - 1]}${blob.path}`);
+			}
+		}
+
+		for (const tree of aTrees) {
+			const inB = bTrees.find(({ path }) => path === tree.path);
+
+			if (!inB) {
+				// Dir was created, we don't count it
+				continue;
+			}
+
+			if (tree.oid !== inB.oid) {
+				await compareTrees(tree.oid, inB.oid, [
+					...prefix,
+					prefix[prefix.length - 1] + tree.path + "/"
+				]);
+			}
+		}
+	};
+
+	const visitedCommits = new Set();
+
+	const walkCommit = async (commit: git.ReadCommitResult) => {
+		if (visitedCommits.has(commit.oid)) {
+			return;
+		}
+		visitedCommits.add(commit.oid);
+
+		const parents = await Promise.all(
+			commit.commit.parent.map(oid => git.readCommit({ dir, oid }))
+		);
+
+		await Promise.all(
+			parents.map(parent =>
+				compareTrees(commit.commit.tree, parent.commit.tree)
+			)
+		);
+
+		await Promise.all(parents.map(walkCommit));
+	};
+
+	const headId = (await git.log({ dir, depth: 1 }))[0].oid;
+	const head = await git.readCommit({ dir, oid: headId });
+	await walkCommit(head);
+
+	const makeTreeNode = async (
+		path: string,
+		name: string,
+		oid: string
+	): Promise<TreeNode> => {
+		const tree = await git.readTree({ dir, oid });
+
+		const children: TreeNode[] = [];
+
+		for (const entry of tree.tree) {
+			if (entry.type === "blob") {
+				children.push({
+					name: entry.path,
+					type: "file",
+					changes: changes.get(path + entry.path) || 0,
+					children: []
+				});
+				continue;
+			}
+
+			if (entry.type === "tree") {
+				children.push(
+					await makeTreeNode(path + entry.path + "/", entry.path, entry.oid)
+				);
+				continue;
+			}
+		}
+
+		return {
+			name,
+			type: "directory",
+			changes: changes.get(path) || 0,
+			children
+		};
+	};
+
+	return await makeTreeNode("/", "", headId);
+};
---
+++ src/views/App.tsx
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from "react";
+import { analyzeRepo, TreeNode, followPath } from "../git";
+
+export const App = () => {
+	const [repo, setRepo] = useState("https://github.com/sathorn6/repo-explorer");
+	const [exploring, setExploring] = useState(false);
+
+	if (exploring) {
+		return <Explore repo={repo} />;
+	}
+
+	return (
+		<div>
+			<h1>Git Repo Explorer</h1>
+			<div>
+				<input
+					type="text"
+					value={repo}
+					onChange={e => setRepo(e.target.value)}
+				/>
+				<button onClick={() => setExploring(true)}>explore</button>
+			</div>
+		</div>
+	);
+};
+
+const Explore = ({ repo }: { repo: string }) => {
+	const [tree, setTree] = useState();
+
+	useEffect(() => {
+		const worker = async () => {
+			setTree(await analyzeRepo(repo));
+		};
+
+		worker();
+	}, [repo]);
+
+	return (
+		<div>
+			Analyzing {repo}
+			{tree && <ResultView root={tree} />}
+		</div>
+	);
+};
+
+const ResultView = ({ root }: { root: TreeNode }) => {
+	const [path, setPath] = useState("");
+	const dirs = path.split("/");
+	dirs.pop();
+	const parent = dirs.join("/");
+
+	const node = followPath(root, path);
+
+	return (
+		<div>
+			<div>
+				<input
+					type="text"
+					value={path}
+					onChange={event => setPath(event.target.value)}
+				/>
+			</div>
+			<PathNavigator path={path} setPath={setPath} rootName="root" />
+			{node && (
+				<TreeView
+					tree={node}
+					onClick={entry => {
+						if (entry.type === "directory") {
+							setPath((path ? path + "/" : "") + entry.name);
+						}
+					}}
+					onGoUp={path ? () => setPath(parent) : undefined}
+				/>
+			)}
+		</div>
+	);
+};
+
+const PathNavigator = ({
+	rootName,
+	path,
+	setPath
+}: {
+	rootName: string;
+	path: string;
+	setPath: (path: string) => void;
+}) => {
+	const dirs = path.split("/");
+	if (dirs[0] === "") {
+		dirs.shift();
+	}
+
+	return (
+		<div>
+			<button onClick={() => setPath("")}>{rootName}</button>
+			{dirs.map((dir, i) => (
+				<button key={i} onClick={() => setPath(dirs.slice(0, i + 1).join("/"))}>
+					{dir}
+				</button>
+			))}
+		</div>
+	);
+};
+
+const TreeView = ({
+	tree,
+	onClick,
+	onGoUp
+}: {
+	tree: TreeNode;
+	onClick: (entry: TreeNode) => void;
+	onGoUp?: () => void;
+}) => {
+	const sortedChildren = Array.from(tree.children).sort((a, b) => {
+		if (a.type === b.type) {
+			return b.changes - a.changes;
+		}
+
+		if (a.type === "directory") {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
+
+	return (
+		<div>
+			<h2>{tree.name}</h2>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th># of changes</th>
+					</tr>
+				</thead>
+				<tbody>
+					{onGoUp && (
+						<tr onClick={onGoUp}>
+							<td colSpan={2}>
+								<button>..</button>
+							</td>
+						</tr>
+					)}
+					{sortedChildren.map((entry, index) => (
+						<tr key={index}>
+							<td>
+								<NodeView node={entry} onClick={() => onClick(entry)} />
+							</td>
+							<td>{entry.changes}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+const NodeView = ({
+	node,
+	onClick
+}: {
+	node: TreeNode;
+	onClick: () => void;
+}) => {
+	switch (node.type) {
+		case "directory":
+			return <button onClick={onClick}>{node.name}</button>;
+		case "file":
+			return <span>{node.name}</span>;
+	}
+};
