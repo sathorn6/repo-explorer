7f240d71a1c3accf17abca2947d6b72528f6a527
sathorn6 <sathorn6@users.noreply.github.com>
2020-02-29 00:46:31 +0100

    Add style

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./views/App";
+import "./tailwind.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import { analyzeRepo, TreeNode, followPath } from "../git";
 
 export const App = () => {
+	return (
+		<div className="p-12 max-w-screen-md mx-auto">
+			<Home />
+		</div>
+	);
+};
+
+export const Home = () => {
 	const [repo, setRepo] = useState("https://github.com/sathorn6/repo-explorer");
 	const [exploring, setExploring] = useState(false);
 
@@ -11,14 +19,43 @@ export const App = () => {
 
 	return (
 		<div>
-			<h1>Git Repo Explorer</h1>
-			<div>
-				<input
-					type="text"
-					value={repo}
-					onChange={e => setRepo(e.target.value)}
+			<svg
+				className="m-auto fill-current text-indigo-600"
+				width="121"
+				height="121"
+				viewBox="0 0 121 121"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M2.94932 53C-0.955922 56.9053 -0.955928 63.2369 2.94931 67.1421L53 117.193C56.9053 121.098 63.2369 121.098 67.1422 117.193L117.193 67.1422C121.098 63.2369 121.098 56.9053 117.193 53L67.1422 2.94932C63.2369 -0.955922 56.9053 -0.955928 53 2.94931L41.6117 14.3376L56.5096 29.2356C57.3531 28.9649 58.2524 28.8188 59.1858 28.8188C64.0182 28.8188 67.9356 32.7362 67.9356 37.5686C67.9356 38.502 67.7894 39.4012 67.5188 40.2447L82.8574 55.5833C84.0856 54.9262 85.489 54.5535 86.9793 54.5535C91.8117 54.5535 95.7292 58.471 95.7292 63.3034C95.7292 68.1357 91.8117 72.0532 86.9793 72.0532C82.1469 72.0532 78.2295 68.1357 78.2295 63.3034C78.2295 62.9721 78.2479 62.6451 78.2838 62.3234L63.7005 47.7401V76.3945C66.2385 77.9263 67.9356 80.7105 67.9356 83.8912C67.9356 88.7236 64.0182 92.641 59.1858 92.641C54.3534 92.641 50.436 88.7236 50.436 83.8912C50.436 80.2975 52.6024 77.2099 55.7005 75.863V45.5967C52.6024 44.2499 50.436 41.1622 50.436 37.5686C50.436 36.6352 50.5821 35.7359 50.8528 34.8924L35.9548 19.9945L2.94932 53Z"
 				/>
-				<button onClick={() => setExploring(true)}>explore</button>
+			</svg>
+			<h1 className="text-5xl text-center text-gray-900 font-black tracking-tight">
+				Git Repo Explorer
+			</h1>
+			<h2 className="text-lg text-center text-gray-600">
+				Get quick insights into any Git repository.
+			</h2>
+			<div className="flex flex-col mt-16">
+				<label className="text-md text-gray-800 font-bold">
+					Git Repository URL:
+				</label>
+				<div className="flex mt-2">
+					<input
+						className="flex-1 p-4 text-base border text-gray-900"
+						type="text"
+						value={repo}
+						onChange={e => setRepo(e.target.value)}
+					/>
+					<button
+						className="ml-4 text-lg py-4 px-8 bg-indigo-600 text-white"
+						onClick={() => setExploring(true)}
+					>
+						Explore
+					</button>
+				</div>
 			</div>
 		</div>
 	);
@@ -35,12 +72,11 @@ const Explore = ({ repo }: { repo: string }) => {
 		worker();
 	}, [repo]);
 
-	return (
-		<div>
-			Analyzing {repo}
-			{tree && <ResultView root={tree} />}
-		</div>
-	);
+	if (tree) {
+		return <ResultView root={tree} />;
+	}
+
+	return <p className="text-center text-gray-500">Analyzing {repo}...</p>;
 };
 
 const ResultView = ({ root }: { root: TreeNode }) => {
@@ -53,13 +89,6 @@ const ResultView = ({ root }: { root: TreeNode }) => {
 
 	return (
 		<div>
-			<div>
-				<input
-					type="text"
-					value={path}
-					onChange={event => setPath(event.target.value)}
-				/>
-			</div>
 			<PathNavigator path={path} setPath={setPath} rootName="root" />
 			{node && (
 				<TreeView
@@ -91,15 +120,56 @@ const PathNavigator = ({
 	}
 
 	return (
-		<div>
-			<button onClick={() => setPath("")}>{rootName}</button>
+		<div className="mb-4">
+			<PathDir currentPath={path} path={""} setPath={setPath}>
+				{rootName}
+			</PathDir>
 			{dirs.map((dir, i) => (
-				<button key={i} onClick={() => setPath(dirs.slice(0, i + 1).join("/"))}>
-					{dir}
-				</button>
+				<React.Fragment key={i}>
+					<span className="mx-2 text-gray-500">/</span>
+					<PathDir
+						currentPath={path}
+						path={dirs.slice(0, i + 1).join("/")}
+						setPath={setPath}
+					>
+						{dir}
+					</PathDir>
+				</React.Fragment>
 			))}
 		</div>
 	);
+};
+
+const PathDir = ({
+	path,
+	currentPath,
+	setPath,
+	children
+}: {
+	path: string;
+	currentPath: string;
+	setPath: (path: string) => void;
+	children: React.ReactNode;
+}) => {
+	const isCurrent = path === currentPath;
+	const isRoot = path === "";
+
+	let className = "text-lg ";
+
+	className += isCurrent ? "text-gray-900" : "text-indigo-500 hover:underline";
+	if (isRoot) {
+		className += " font-bold";
+	}
+
+	if (isCurrent) {
+		return <span className={className}>{children}</span>;
+	} else {
+		return (
+			<button onClick={() => setPath(path)} className={className}>
+				{children}
+			</button>
+		);
+	}
 };
 
 const TreeView = ({
@@ -124,36 +194,38 @@ const TreeView = ({
 	});
 
 	return (
-		<div>
-			<h2>{tree.name}</h2>
-			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th># of changes</th>
-						<th># of files</th>
+		<table className="w-full bg-white">
+			<thead>
+				<tr className="border bg-gray-200">
+					<th className="w-full px-4 py-2 text-left">Name</th>
+					<th className="whitespace-no-wrap px-4 py-2"># of changes</th>
+					<th className="whitespace-no-wrap px-4 py-2"># of files</th>
+				</tr>
+			</thead>
+			<tbody>
+				{onGoUp && (
+					<tr className="border">
+						<td colSpan={3} className="px-4 py-2">
+							<button
+								className="px-1 py-1 -my-1 hover:bg-indigo-200 rounded text-indigo-500 hover:underline"
+								onClick={onGoUp}
+							>
+								..
+							</button>
+						</td>
 					</tr>
-				</thead>
-				<tbody>
-					{onGoUp && (
-						<tr onClick={onGoUp}>
-							<td colSpan={2}>
-								<button>..</button>
-							</td>
-						</tr>
-					)}
-					{sortedChildren.map((entry, index) => (
-						<tr key={index}>
-							<td>
-								<NodeView node={entry} onClick={() => onClick(entry)} />
-							</td>
-							<td>{entry.numChanges}</td>
-							<td>{entry.numFiles}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+				)}
+				{sortedChildren.map((entry, index) => (
+					<tr key={index} className="border">
+						<td className="px-4 py-2">
+							<NodeView node={entry} onClick={() => onClick(entry)} />
+						</td>
+						<td className="px-4 py-2 text-right">{entry.numChanges}</td>
+						<td className="px-4 py-2 text-right">{entry.numFiles}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
 	);
 };
 
@@ -166,7 +238,11 @@ const NodeView = ({
 }) => {
 	switch (node.type) {
 		case "directory":
-			return <button onClick={onClick}>{node.name}</button>;
+			return (
+				<button className="text-indigo-500 hover:underline" onClick={onClick}>
+					{node.name}
+				</button>
+			);
 		case "file":
 			return <span>{node.name}</span>;
 	}
