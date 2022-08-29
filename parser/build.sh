efe0d584eeb8a7922316cb420780c7e1d36f405b
sathorn6 <sathorn6@users.noreply.github.com>
2022-08-30 00:36:15 +0100

    Make build script more robust

--- parser/build.sh
+++ parser/build.sh
@@ -4,7 +4,8 @@ set -e
 
 wasm-pack build --target web
 # Remove this line because we don't need it and it doesn't work our my environment
-sed -i "/import\.meta/d" pkg/rsgit.js
+sed -i.bak "/import\.meta/d" pkg/rsgit.js && rm pkg/rsgit.js.bak
+mkdir -p ../dist/
 cp pkg/rsgit_bg.wasm ../dist/
 rm -rf ../src/pkg
 cp -r pkg ../src/pkg
